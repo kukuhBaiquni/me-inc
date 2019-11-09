ef0213c9a30907d948ff38fe15e6dcf88cc279ce
unknown <kukuh.baiquni@gmail.com>
2019-11-09 10:50:58 +0700

    scss variable & fonts

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./Header.scss";
 
 export default class Header extends Component {
     render() {
         return (
-            <div>
+            <div className="header-main">
                 Header
             </div>
         )

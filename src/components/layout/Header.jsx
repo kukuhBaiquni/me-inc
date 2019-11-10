4c152dc362e8e5d25eeb218551ecb663ec367594
unknown <kukuh.baiquni@gmail.com>
2019-11-10 19:33:48 +0700

    Data on modal + editing

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,21 +1,44 @@
 import React, { Component } from "react";
 import "./Header.scss";
-import { Menu, Button, Icon } from "antd";
-const { SubMenu } = Menu;
+import { Menu, Button, Icon, Drawer } from "antd";
+import { menu } from "../../constant/enums";
+import { Link } from "react-router-dom";
+
 export default class Header extends Component {
-    state = {
-        current: "mail"
+    constructor(props) {
+        super(props)
+        this.state = {
+            isDrawerVisible: false
+        }
     };
 
-    handleClick = e => {
-        console.log("click ", e);
-        this.setState({
-            current: e.key
-        });
-    };
+    _showDrawer = () => this.setState({isDrawerVisible: true});
+    _closeDrawer = () => this.setState({isDrawerVisible: false});
+
     render() {
+        const { isDrawerVisible } = this.state;
         return (
             <div className="header-main">
+                <Drawer
+                    title="Menu List"
+                    placement="left"
+                    closable={false}
+                    onClose={this._closeDrawer}
+                    visible={isDrawerVisible}
+                >
+                    {
+                        menu.map((x, i) => (
+                            <Link to={x.path} key={i}>
+                                <div className="menu-list">
+                                    {x.title}
+                                </div>
+                            </Link>
+                        ))
+                    }       
+                </Drawer>
+                <div className="left-menu">
+                    <Icon onClick={this._showDrawer} type="menu" style={{color: "white", fontSize: "24px"}} />
+                </div>
                 <div className="user-info">
                     <div className="prop">
                         Username <br/>
@@ -36,4 +59,4 @@ export default class Header extends Component {
             </div>
         );
     }
-}
+};
--- src/page/pos/Pos.jsx
+++ src/page/pos/Pos.jsx
@@ -1,15 +1,16 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import "./Pos.scss";
-import { Table, Divider, Tag, Button, Select, Col, Row, Input, Modal } from "antd";
+import { Table, Divider, Tag, Button, Select, Col, Row, Input, Modal, InputNumber } from "antd";
 import nanoid from "nanoid";
+import moment from "moment";
 
-export default class Pos extends Component {
+export default class Pos extends PureComponent {
     constructor(props) {
         super(props)
         this.state = {
             isModalVisible: false,
             loading: false,
-            currentTarget: null
+            details: null
         }
     };
 
@@ -23,12 +24,12 @@ export default class Pos extends Component {
     };
 
     _tjimenQ = (evt, data, index) => {
-        this.setState({currentTarget: index});
+        this.setState({details: data});
         this._showModal();
     };
-
+    
     render() {
-        const { isModalVisible, loading, currentTarget } = this.state;
+        const { isModalVisible, loading, details } = this.state;
         return (
             <div className="pos-main">
                 <ModalDetail 
@@ -37,7 +38,7 @@ export default class Pos extends Component {
                     onSubmit={this._handleOk}
                     isModalVisible={isModalVisible}
                     loading={loading}
-                    data={data[currentTarget]}
+                    data={details}
                 />
                 <div className="content-wrapper">
                     Transaction List
@@ -83,27 +84,145 @@ export default class Pos extends Component {
     }
 };
 
-const ModalDetail = (props) => (
-    <Modal
-        visible={props.isModalVisible}
-        title="Title"
-        onOk={props.onSubmit}
-        onCancel={props.closeModal}
-        footer={[
-            <Button key="back" onClick={props.closeModal}>
-              Return
-            </Button>,
-            <Button key="submit" type="primary" loading={props.loading} onClick={props.onSubmit}>
-              Submit
-            </Button>,
-          ]}
-        >
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-    </Modal>
-)
+class ModalDetail extends PureComponent {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isEditing: false
+        };
+    };
+
+    componentDidUpdate(prevProps) {
+        if(prevProps.isModalVisible !== this.props.isModalVisible) {
+            if(this.state.isEditing) {
+                this.setState({isEditing: false});
+            }
+        }
+    };
+
+    _editData = () => this.setState({isEditing: true});
+    _cancelEdit = () => this.setState({isEditing: false});
+
+    render() {
+        const { onSubmit, closeModal, data, isModalVisible, loading } = this.props;
+        const { isEditing } = this.state;
+        if(isEditing) {
+            return(
+                <Modal
+                    visible={isModalVisible}
+                    title="EDIT TRANSACTION"
+                    onOk={onSubmit}
+                    onCancel={closeModal}
+                    width="40%"
+                    footer={[
+                        <Button key="back" onClick={this._cancelEdit}>
+                            Cancel
+                        </Button>,
+                        <Button key="save" style={{backgroundColor: "green", color: "white"}} loading={loading} onClick={onSubmit}>
+                            Save
+                        </Button>
+                    ]}
+                    >
+                    {
+                        data &&
+                        <>
+                            <label>TRX</label>
+                            <Input id="trx" defaultValue={data.trx} disabled />
+                            <label>Name</label>
+                            <Input id="name" defaultValue={data.name} disabled />
+                            <label>Detail items</label>
+                            {
+                                dataTable.map((x, i) => (
+                                    <Row className="row-loop">
+                                        <Col md={1}>
+                                            <label>{i + 1}</label>
+                                        </Col>
+                                        <Col md={5}>
+                                            <Row>
+                                                <label>Product Name</label>
+                                            </Row>
+                                            <Row>
+                                                <Select id="productName" defaultValue="lucy" className="select-product">
+                                                    <Select.Option value="name">Air Gabon</Select.Option>
+                                                    <Select.Option value="lucy">Air Mesir</Select.Option>
+                                                    <Select.Option value="Yiminghe">Air Gunung</Select.Option>
+                                                </Select>
+                                            </Row>
+                                        </Col>
+                                        <Col md={1} />
+                                        <Col md={5}>
+                                            <Row>
+                                                <label>Quantity</label>
+                                            </Row>
+                                            <Row>
+                                                <Input defaultValue={x.qty} maxLength={2} className="product-qty" />
+                                            </Row>
+                                        </Col>
+                                        <Col md={1} />
+                                        <Col md={5}>
+                                            <Row>
+                                                <label>Price</label>
+                                            </Row>
+                                            <Row>
+                                                <Input disabled defaultValue={x.price} maxLength={2} className="product-qty" />
+                                            </Row>
+                                        </Col>
+                                        <Col md={1} />
+                                        <Col md={5}>
+                                            <Row>
+                                                <label>Subtotal</label>
+                                            </Row>
+                                            <Row>
+                                                <Input disabled defaultValue={x.subtotal} maxLength={2} className="product-qty" />
+                                            </Row>
+                                        </Col>
+                                    </Row>
+                                ))
+                            }
+                        </>
+                    }
+                </Modal>
+            )
+        }else{
+            return(
+                <Modal
+                    visible={isModalVisible}
+                    title="TRANSACTION DETAILS"
+                    onOk={onSubmit}
+                    onCancel={closeModal}
+                    width="40%"
+                    footer={[
+                        <Button key="back" onClick={this._editData}>
+                            Edit
+                        </Button>,
+                        <Button key="submit" type="danger" loading={loading} onClick={onSubmit}>
+                            Delete
+                        </Button>,
+                    ]}
+                    >
+                    {
+                        data &&
+                        <>
+                            <label>TRX</label>
+                            <p>{data.trx}</p>
+                            <label>Name</label>
+                            <p>{data.name}</p>
+                            <label>Detail items</label>
+                            <Table
+                                bordered={true}
+                                columns={columnTable}
+                                dataSource={dataTable}
+                                pagination={false}
+                            />
+                            <label>Total</label>
+                            <p>{data.amount}</p>
+                        </>
+                    }
+                </Modal>
+            )
+        }
+    }
+}
 
 const columns = [
     {
@@ -151,9 +270,20 @@ const columns = [
     //     )
     // },
     {
-        title: "Sales",
-        dataIndex: "age",
-        key: "age"
+        title: "Status",
+        dataIndex: "status",
+        key: "status",
+        render: status => status ? <p style={{color: "green"}}>Success</p> : <p style={{color: "yellow"}}>Pending</p>
+    },
+    {
+        title: "Amount",
+        dataIndex: "amount",
+        key: "amount"
+    },
+    {
+        title: "Time",
+        dataIndex: "time",
+        key: "time"
     }
 ];
 
@@ -163,9 +293,49 @@ const data = Array(425)
         return {
             key: i + 1,
             name: "TjimenQ",
-            age: Math.ceil(Math.random() * 76),
+            amount: Math.ceil(Math.random() * 76),
             address: "Jl. kaki",
-            tags: ["SempaQ", "KutanQ"],
-            trx: nanoid(10)
+            items: ["SempaQ", "KutanQ"],
+            trx: nanoid(10),
+            time: moment().format("DD MMM YYYY - HH:mm"),
+            status: Math.random() * 100 >= 50 ? true : false
         };
     });
+
+    const columnTable = [
+        {
+            title: "No",
+            dataIndex: "key",
+            key: "key"
+        },
+        {
+            title: "Product Name",
+            dataIndex: "name",
+            key: "name"
+        },
+        {
+            title: "Price",
+            dataIndex: "price",
+            key: "price"
+        },
+        {
+            title: "Quantity",
+            dataIndex: "qty",
+            key: "qty"
+        },
+        {
+            title: "Subtotal",
+            dataIndex: "subtotal",
+            key: "subtotal"
+        }
+    ];
+
+    const dataTable = Array(4).fill("TjimenQ").map((x ,i) => {
+        return {
+            key: i + 1,
+            name: "Product isi ulang air" + (i + 1),
+            qty: Math.ceil(Math.random() * 5),
+            subtotal: "Rp. 25.000,-",
+            price: "Rp. 5000,-"
+        }
+    })
